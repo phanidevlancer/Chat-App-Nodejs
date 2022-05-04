6888d682794ec2a12deb542b0ca326413dbfe52a
phanindra <phanidevlancer@gmail.com>
2022-05-04 09:49:41 +0530

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require('path')
 const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname,'../public')
 const port = process.env.PORT || 3000
-var app = express()
+let app = express()
+let server = http.createServer(app)
+let io = socketIO(server)
 
+
+io.on('connection', (socket) => {
+    console.log("A new user just connect");
+
+    socket.on('disconnect', () => {
+        console.log("User was disconnected");
+    })
+})
 app.use(express.static(publicPath))
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Service is up on port ${port}`);
 })
